b0cbe00d1ea77419242359e93d384d3096cfb462
Legatofseal <legatofseal@gmail.com>
2016-04-19 11:58:34 +0300

    Initial commit

---
+++ src/com/company/ArrayCollectionTest.java
@@ -0,0 +1,46 @@
+package com.company;
+
+/**
+ * Created by Legat on 16.04.2016.
+ */
+public class ArrayCollectionTest {
+    int capacity = 500;
+    int size = 0;
+    Object[] internalArray;
+
+
+    public ArrayCollectionTest() {
+        this.internalArray = new Object[capacity];
+
+    }
+
+    public ArrayCollectionTest(int capacity) {
+        this.capacity = capacity;
+        this.internalArray = new Object[capacity];
+    }
+///test
+    public int getSize() {
+        return size;
+    }
+
+    public void AddElement(Object object) {
+        if (size < capacity) {
+            internalArray[size] = object;
+        } else {
+            reSizeArray();
+            internalArray[size] = object;
+        }
+        ++size;
+    }
+
+    public void reSizeArray() {
+        capacity = capacity * 2;
+        Object[] tempArray = new Object[capacity];
+        for (int i = 0; i < size; i++) {
+            tempArray[i] = internalArray[i];
+        }
+        internalArray = tempArray;
+
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.lang.String;
+
+public class Main {
+
+
+
+
+
+    public static void main(String[] args) {
+        System.out.print("Hello");
+
+
+    }
+}
