60c79707d29231109c96d8b498997904f5c4d710
Legatofseal <legatofseal@gmail.com>
2016-04-19 15:36:37 +0300

    MyCheck

--- src/com/company/ArrayCollectionTest.java
+++ src/com/company/ArrayCollectionTest.java
@@ -69,4 +69,25 @@ public class ArrayCollectionTest {
 
 
     }
+
+    public int[] sortArray(int[] array) {
+        int n = array.length;
+        int temp;
+        boolean flag;
+        for (int i = 0; i < n-1; i++) {
+            flag = false;
+            for (int j=0; j<n-i-1; j++){
+                if (array[j]>array[j+1]){
+                    temp = array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=temp;
+                    flag=false;
+                }
+            }
+            if (!flag)
+                break;
+
+        }
+        return array;
+    }
 }
